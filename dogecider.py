47c691781e69a668fbe2682bd7a24cef9a07c982
crocodyluspontifex <chathika@ckw-vostro.(none)>
2014-02-25 22:08:38 +0000

    Welcome message. First raw input.

--- dogecider.py
+++ dogecider.py
@@ -2,8 +2,40 @@
 
 """
 Dogecider v0.1
-(c) Dhruv Nagpal / Chathika Weerasuriya
+(c) 2014 Dhruv Nagpal / Chathika Weerasuriya
 Released under the XXX License
 """
 
 import math
+
+# Welcome message
+
+print """
+
+***********************************************
+
+Welcome to the dogecider v0.1 - the decision making script for indecisive people.
+This simple script will enable you and your lackadaisical friends to choose one of any given list of options in a weighted pseudo-random manner.
+
+***********************************************
+
+The princple is simple:
+1. Enter the number of people in the group.
+2. Each person enters the options they offer, along with a preference score for each option.
+3. The program makes a weighted pseudo-random decision as to what option you're all going to follow through.
+
+Note: it is pretty easy to weight a choice towards what you want by giving it a high preference score - but if you already have a strong preference for a give option, then why the hell are you using the dogecider?
+
+Such indecision. Much solution. Wow.
+Start ----------------------------------->
+"""
+
+group_size = int(raw_input("Enter the number of people trying to make a decision: "))
+print "\n" + str(group_size) + " jokers can't make a decision to save their lives"
+
+
+
+
+print "\n"
+
+exit
